2d3e655c7c4fd1ce71fa521db138c47234b072dd
Rene Ederer <reneederer@arcor.de>
2015-11-14 10:11:34 +0100

    Can drag transparent canvas on the screen

---
+++ javascript.js
@@ -0,0 +1,102 @@
+var textTypes = 
+{
+    CONDITION: 1,
+    STORY: 2
+};
+
+var dragCanvas = {canvas: null, x: 0, y: 0};
+
+$(function()
+{
+    var el = {id: 0, x: 20, y: 20, type: textTypes.STORY, name: "tes1t"};
+    draw(el);
+    el = {id: 1, x: 20, y: 280, type: textTypes.CONDITION, name: "tes2t"};
+    draw(el);
+    el = {id: 2, x: 420, y: 20, type: textTypes.CONDITION, name: "tes3t"};
+    draw(el);
+});
+
+
+function draw(el)
+{
+    $("#main").append("<canvas id='canvas" + el.id + "' style='position: absolute; z-index: " + el.id + ";'></canvas>"); 
+    var c = $("#canvas" + el.id)[0];
+    //$('#canvas' + el.id).on('click', function(){alert(el.id);});
+    c.width = document.body.clientWidth;
+    c.height = document.body.clientHeight;
+    var ctx = c.getContext("2d");
+    var d = $("#papitems")[0];
+
+    var k = JSON.stringify(el);
+    var e = $("#book");
+    var padding = 
+    {
+        LEFT:  18,
+        RIGHT: 18,
+        TOP:   18,
+        BOTTOM:18
+    }
+
+    ctx.font = d.style.font;
+    ctx.font = "1em arial";
+    fontSize = parseInt(ctx.font);
+    var metrics = ctx.measureText(el.name);
+    var width = metrics.width*2 + padding.LEFT + padding.RIGHT;
+    var height = fontSize + padding.TOP + padding.BOTTOM;
+    c.width = width + 10;
+    c.height = (el.type == textTypes.CONDITION) ? 10 + height * 1.2 : 10 + height;
+    c.style.width=c.width + 10;
+    c.style.height = c.height + 10;
+    c.style.left = el.x;
+    c.style.top = el.y;
+    el.x = 5;
+    el.y = 5;
+    ctx = c.getContext("2d");
+    ctx.font = "1em arial";
+
+
+    $(document).mouseup(function(){
+        dragCanvas.canvas = null;
+    }).mousemove(function(e) {
+            if(dragCanvas.canvas != null)
+            {
+                dragCanvas.canvas.style.left = e.pageX - dragCanvas.x; 
+                dragCanvas.canvas.style.top = e.pageY - dragCanvas.y;
+            }
+
+             })
+    $("#canvas" + el.id)
+        .mousedown(function(e) {
+            //TODO make this element the topmost element
+            dragCanvas.canvas = $(this)[0];
+            dragCanvas.x = e.pageX - parseInt($(this)[0].style.left);
+            dragCanvas.y = e.pageY - parseInt($(this)[0].style.top);
+        })
+
+    .mouseup(function() {
+        dragCanvas.canvas = null;
+    });
+    ctx.fillStyle="rgba(0, 0, 200, 0)";
+    ctx.fill();
+
+    ctx.strokeStyle = 'rgba(0,0,0, 1)';
+    ctx.fillStyle="rgba(0, 0, 0, 1)";
+    if(el.type == textTypes.CONDITION)
+    {
+        height *= 1.20;
+        ctx.moveTo(el.x, el.y + height/2);
+        ctx.lineTo(el.x + width/2, el.y);
+        ctx.lineTo(el.x + width, el.y + height/2);
+        ctx.lineTo(el.x + width/2, el.y + height);
+        ctx.lineTo(el.x, el.y + height/2);
+        ctx.fillText(el.name, el.x + metrics.width / 2 + padding.LEFT, el.y + height/2 + fontSize/2 - 0.2*fontSize);
+    }
+    else if(el.type == textTypes.STORY)
+    {
+        ctx.rect(el.x, el.y, width, height);
+        ctx.fillText(el.name, el.x + metrics.width/2 + padding.LEFT, el.y + height - 0.20*fontSize - padding.BOTTOM);
+    }
+    ctx.stroke();
+}
+
+
--- pap.php
+++ pap.php
@@ -18,57 +18,16 @@
             }
         </style>
     </head>
-    <body onresize='draw(20, 20, 1, "adfsjkasld;f")"'>
-        <canvas id="main"></canvas>
+    <body>
+        <div id="main" style="background-color: yellow;"></div>
         <div id="papitems">
         </div>
-<script type="text/javascript">
-var textTypes = 
-{
-    CONDITION: 1,
-    STORY: 2
-};
-var c = document.getElementById("main");
-c.width = document.body.clientWidth;
-c.height = document.body.clientHeight;
-var ctx = c.getContext("2d");
-var d = document.getElementById("papitems");
+        <form id="book">
+            <input type="hidden" name="elements[]" />
+        </form>
+        <canvas id="throbble"></canvas>
+        <script src="jquery.js"></script>
+        <script src="javascript.js"></script>
 
-draw(480, 20, textTypes.STORY, "dfkasld;gggg;");
-draw(480, 130, textTypes.CONDITION, "Was essen?");
-ctx.stroke();
-function draw(x, y, type, name)
-{
-    var padding = 
-    {
-        LEFT:  28,
-        RIGHT: 28,
-        TOP:   28,
-        BOTTOM:28
-    }
-    
-    ctx.font = d.style.font;
-    ctx.font = "1em arial";
-    fontSize = parseInt(ctx.font);
-    var metrics = ctx.measureText(name);
-    var width = metrics.width*2 + padding.LEFT + padding.RIGHT;
-    var height = fontSize + padding.TOP + padding.BOTTOM;
-    if(type == textTypes.CONDITION)
-    {
-        height *= 1.20;
-        ctx.moveTo(x, y + height/2);
-        ctx.lineTo(x + width/2, y);
-        ctx.lineTo(x + width, y + height/2);
-        ctx.lineTo(x + width/2, y + height);
-        ctx.lineTo(x, y + height/2);
-        ctx.fillText(name, x + metrics.width / 2 + padding.LEFT, y + height/2 + fontSize/2 - 0.2*fontSize);
-    }
-    else if(type == textTypes.STORY)
-    {
-        ctx.rect(x, y, width, height);
-        ctx.fillText(name, x + metrics.width/2 + padding.LEFT, y + height - 0.20*fontSize - padding.BOTTOM);
-    }
-}
-</script>
     </body>
 </html>
