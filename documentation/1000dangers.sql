1d93b3c0c55f068295597fb6a96418a021ae45d5
Rene Ederer <reneederer@arcor.de>
2015-11-22 23:21:41 +0100

    Fixed a composed foreign key constraint

--- documentation/1000dangers.sql
+++ documentation/1000dangers.sql
@@ -5,7 +5,7 @@ create table team(id int auto_increment primary key, name varchar(50), password 
 create table 1000dangersbook(id int auto_increment primary key, team_id int, name varchar(150), creationDate datetime, foreign key(team_id) references team(id));
 create table paptype(id int auto_increment primary key, name varchar(30));
 create table papelement(id int, 1000dangersbook_id int, paptype_id int, x int, y int, title varchar(200), text text, primary key(id, 1000dangersbook_id), foreign key(1000dangersbook_id) references 1000dangersbook(id), foreign key(paptype_id) references paptype(id));
-create table papconnection(source_id int, destination_id int, 1000dangersbook_id int, source_offset_x int, source_offset_y int, destination_offset_x int, destination_offset_y int, title varchar(200), primary key(source_id, destination_id, 1000dangersbook_id), foreign key(source_id) references papelement(id), foreign key(destination_id) references papelement(id), foreign key(1000dangersbook_id) references 1000dangersbook(id));
+create table papconnection(source_id int, destination_id int, 1000dangersbook_id int, source_offset_x int, source_offset_y int, destination_offset_x int, destination_offset_y int, title varchar(200), primary key(source_id, destination_id, 1000dangersbook_id), foreign key(source_id, 1000dangersbook_id) references papelement(id, 1000dangersbook_id), foreign key(destination_id, 1000dangersbook_id) references papelement(id, 1000dangersbook_id));
 insert into team(id, name, password) values(1, "rene", "1234");
 insert into 1000dangersbook(id, team_id, name, creationdate) values(1, 1, "1000 Gefahren", now());
 insert into paptype(id, name) values(1, "Start"), (2, "End"), (3, "Action"), (4, "Condition");
