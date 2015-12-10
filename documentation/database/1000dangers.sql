792c08751036dab949e5c6c55b3fb643adf8734f
Rene Ederer <reneederer@arcor.de>
2015-12-11 00:18:23 +0100

    Created a better story

--- documentation/database/1000dangers.sql
+++ documentation/database/1000dangers.sql
@@ -12,12 +12,17 @@ create table papconnection(source_id int, destination_id int, 1000dangersbook_id
 insert into team(id, name, password) values(1, "rene", "1234");
 insert into 1000dangersbook(id, team_id, name, creationdate) values(1, 1, "1000 Gefahren", now());
 insert into paptype(id, name) values(1, "Start"), (2, "End"), (3, "Action"), (4, "Condition");
-insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(1, 1, 1, 20, 200, "Start", "");
-insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(2, 1, 2, 60, 300, "Ende", "");
-insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(3, 1, 3, 180, 400, "Insel gestrandet", "Du hast Schiffbruch erlitten. Nachdem du dich auf eine einsame Insel gerettet hast, begegnen dir 5 Kannibalen.");
-insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(4, 1, 4, 180, 400, "Kannibalen kennenlernen?", "Moechtest du dich erstmal mit den Kannibalen anfreunden?");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(1, 1, 1, 517, 6, "Start", "");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(2, 1, 2, 535, 525, "Ende", "");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(3, 1, 3, 502, 159, "Insel gestrandet", "Du hast Schiffbruch erlitten. Nachdem du dich auf eine einsame Insel gerettet hast, begegnen dir 5 Kannibalen.");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(4, 1, 4, 797, 85, "Kannibalen kennenlernen?", "Moechtest du dich erstmal mit den Kannibalen anfreunden?");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(5, 1, 3, 1015, 333, "Vor Kannibalen fliehen", "Du hast dich dazu entschieden, vor, vor den Kannibalen zu fliehen. Auf der Flucht stolperst du und verblutest qualvoll.");
+insert into papelement(id, 1000dangersbook_id, paptype_id, x, y, title, text) values(6, 1, 3, 586, 398, "Mit Kannibalen anfreunden", "Du lernst die Kannibalen kennen. Sie sind viel netter, als man sonst so liest. Sie organisieren fuer dich die Heimreise. 6 Wochen spaeter bist du zu Hause.");
+insert into papconnection(source_id, destination_id, 1000dangersbook_id, source_offset_x, source_offset_y, destination_offset_x, destination_offset_y, title) values(4, 6, 1, 30, 150, 20, 20, "ja");
+/*
 insert into papconnection(source_id, destination_id, 1000dangersbook_id, source_offset_x, source_offset_y, destination_offset_x, destination_offset_y, title) values(1, 2, 1, 0, 0, 0, 0, "Eine Verbindung");
 insert into papconnection(source_id, destination_id, 1000dangersbook_id, source_offset_x, source_offset_y, destination_offset_x, destination_offset_y, title) values(1, 3, 1, 0, 0, 0, 0, "Titel der Verbindung");
 insert into papconnection(source_id, destination_id, 1000dangersbook_id, source_offset_x, source_offset_y, destination_offset_x, destination_offset_y, title) values(2, 4, 1, 0, 0, 0, 0, "Verbindungstitel");
+*/
  
 
